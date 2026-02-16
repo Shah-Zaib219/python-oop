9639e35280fdb44999cf7e5207edab1f850095fe
Shah-Zaib219 <shahzaib63219@gmail.com>
2026-02-16 16:54:37 +0500

    update

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def evenodd(number):
-    if(number%2==0):
-        print(number,'Number is Even')
-    else:
-        print(number,'Number is Odd')
-print("Assalamualikum")
-numbers=[1,2,3,4,5,6,7,8]
-print(numbers)
-for number in numbers:
-    evenodd(number)
+# name=input("Enter you name: ")
+# print("Welcome"+name)
+# print(type(name))
+# num1=int(input('enter 1st number: '))
+# num2=int(input('enter 2nd number: '))
+# print(num1+num2)
