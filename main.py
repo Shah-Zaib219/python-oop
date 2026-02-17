c4c94cbe250eb0dfd26a24e874b7456dd056700c
Shah-Zaib219 <shahzaib63219@gmail.com>
2026-02-17 12:27:50 +0500

    update

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
-# name=input("Enter you name: ")
-# print("Welcome"+name)
-# print(type(name))
-# num1=int(input('enter 1st number: '))
-# num2=int(input('enter 2nd number: '))
-# print(num1+num2)
-list1=['Shah', 'zaib']
-print(list1)
-name=list1[0]+" " +list1[1]
-print(name)
-tuple1=("one", "two", "one")
-print(tuple1)
-list1.append('shah zaib')
-print(list1)
+##################          #Level ONE
+print('Hello Python')
+name=input('Enter your name: ')
+print(f'Welcome {name}')
+age=21
+city='Abbottabad'
+print(f'Welcome {name}, from {city}, age is {age}')
+print(type(10), type(10.5), type('zaib'), type(True))
+a=10
+b=20
+a,b=b,a
+print(a,b)
+num1=int(input('Enter 1st number:'))
+num2=int(input('Enter 2nd number: '))
+print(f'Sum is {num1+num2}')
+print(f'Differnece is {num1-num2}')
+print(f'Multiplication {num1*num2}')
+print(f'Division {num1/num2}')
+
+
+num3=int(input('Enter a number:'))
+if num3%2==0:
+    print('Even')
+else:
+    print('odd')
+
+num4=int(input('Enter a number:'))
+num5=int(input('Enter another number:'))
+if num4>num5:
+    print(f'{num4} is greater than {num5}')
+elif num5>num4:
+    print(f'{num5} is greater than {num4}')
+
+num6=int(input("Enter value"))
+print(2*3.14*(num6**2))
+
+
