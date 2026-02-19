5f49b6093bf1543bde821f6e03da9d8d494d084e
Shah-Zaib219 <shahzaib63219@gmail.com>
2026-02-19 09:28:02 +0500

    update

--- main.py
+++ main.py
@@ -116,34 +116,209 @@
 # print(s10.strip())
 
 
-#########     LEVEL 1 — Basic Logic + Loops + Lists
+# #########     LEVEL 1 — Basic Logic + Loops + Lists
 
-for i in range(1,101):
-    print(i)
-
-
-for i in range(1,51):
-    if i%2==0:
-        print(i)
-
-num=int(input('Enter a number: '))
-total=0;
-for i in range(1, num):
-    total+=i
-print(total)
-
-num1=int(input('Enter a number: '))
-for i in range(1, num1+1):
-    print(f"{i} x {num1} = {i*num1}")
+# for i in range(1,101):
+#     print(i)
 
 
-num2=int(input('Enter a number: '))
-count=0
-while True:
-    if num2<=9:
-        count+=1
-        break
-    else:
-        num2=num2//10
-        count+=1
-print(count)
+# for i in range(1,51):
+#     if i%2==0:
+#         print(i)
+
+# num=int(input('Enter a number: '))
+# total=0;
+# for i in range(1, num):
+#     total+=i
+# print(total)
+
+# num1=int(input('Enter a number: '))
+# for i in range(1, num1+1):
+#     print(f"{i} x {num1} = {i*num1}")
+
+
+# num2=int(input('Enter a number: '))
+# count=0
+# while True:
+#     if num2<=9:
+#         count+=1
+#         break
+#     else:
+#         num2=num2//10
+#         count+=1
+# print(count)
+
+#### 6
+# list1=[4,5,7,8,6]
+# sum1=0
+# avg=0
+# largest=list1[0]
+# smallest=list1[0]
+# for num in list1:
+#     sum1+=num
+#     if num>largest:
+#         largest=num
+#     if num<smallest:
+#         smallest=num
+
+# avg=sum1/len(list1)
+# print(f"Sum: {sum1}, Average: {avg}, Largest: {largest}, Smallest: {smallest}")
+
+##### 7 8
+# list2=list()
+# even_count=0
+# odd_count=0
+# for i in range(1, 11):
+#     num3=int(input('Enter a number: '))
+#     if num3%2==0:
+#         even_count+=1
+#     else:
+#         odd_count+=1
+#     list2.append(num3)
+# print(list2)
+# print("Even Counts: ", even_count)
+# print("Odd Count: ", odd_count)
+
+# ### 9
+# list3=[4,6,9,1,77,9,5,2,99,0,889,5,6,8]
+# maximum=list3[0]
+# for num in list3:
+#     if num>maximum:
+#         maximum=num
+# print(maximum)
+
+
+
+# ###### 10
+
+# list4=list()
+# for i in range(len(list3), 0, -1):
+#     list4.append(list3[i-1])
+# print(list4)
+
+# ######## 11
+# num4=int(input("Ener a number: "))
+# half=num4//2
+# print(half)
+# for i in range(2, half):
+#     if num4%i==0:
+#         print("Not Prime")
+#         break
+
+# ############# 12
+
+# list5 = [44,4,4,6,9,1,77,9,5,2,99,0,889,5,6,8]
+# list6 = []
+
+# for item in list5:
+#     if item not in list6:
+#         list6.append(item)
+
+# print(list6)
+
+
+
+#### 15
+
+# list7 = [80, 65, 45, 76, 7, 3, 45, 6]
+
+# largest = second_largest = float('-inf')  # initialize to very small number
+# print(largest)
+# for i in list7:
+#     if i > largest:
+#         second_largest = largest
+#         largest = i
+#     elif i > second_largest and i != largest:
+#         second_largest = i
+
+# print("Largest:", largest)
+# print("Second Largest:", second_largest)
+
+
+# #### 17
+# list8=[2,5,3,4,7,8,9,0,87,6,77,5,2,33,1,21,2,3,5,7,5,4,5,4,443,3,24,67,5,78,4]
+
+# even_list8=[]
+# odd_list8=[]
+
+# for i in list8:
+#     if i%2==0:
+#         even_list8.append(i)
+#     else:
+#         odd_list8.append(i)
+
+# print(even_list8)
+# print(odd_list8)
+
+
+# ##### 19
+# name = "shah zaib"
+
+# print(f"{name.count('a')} {name.count('e')} {name.count('i')} {name.count('o')} {name.count('u')}")
+
+
+# str1="DEesED"
+# flag=True
+# for i in range(0,len(str1)//2):
+#     j=len(str1)-i-1
+#     if str1[i]==str1[j]:
+#         flag=True
+#     else:
+#         flag=False
+#         break
+# if flag:
+#     print('Palindome')
+# else:
+#     print('Not Palindrome')
+
+
+
+# ######################  22
+# for i in range(1,5):
+#     for j in range(0,i):
+#         print("*", end="")
+#     print('')
+
+
+
+# ############     23
+# for i in range(1,11):
+#     for j in range(1, 10):
+#         print(f"{i} * {j} = {i*j}")
+
+
+# ##### 24
+# student={
+#     'name': 'Shah Zaib',
+#     'age': 23,
+#     'marks': 3.71
+# }
+# print(student)
+
+
+# #######   25
+# student['city']="Abbottabad"
+# print(student)
+
+
+# ######## 26
+# student['marks']=3.75
+# print(student)
+
+
+
+# ###### 27
+# student.pop('city')
+# print(student)
+
+
+
+######## 28
+
+list28=[1,2,1,3,4,5,4,3,6,7,5,3,1,2,4,2,5,6,4,2,5,6,3,5,2,4,6,7,4]
+numbers_count={}
+for i in range(0, len(list28)):
+    if i not in numbers_count:
+        n=list28.count(i)
+        numbers_count[i]=n
+print(n)
