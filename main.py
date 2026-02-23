23f78851f50365d4da5e006c860cd5adbd97471f
Shah-Zaib219 <shahzaib63219@gmail.com>
2026-02-23 15:28:15 +0500

    update

--- main.py
+++ main.py
@@ -428,17 +428,3 @@
 #     num1=int(input("Enter a number: "))
 #     return num1*num1
 # print(square())
-
-class mathmetical_options:
-    def __init__(self, name):
-        self.name=name
-        print(f'Welcome {name}')
-    def sum(self,a,b):
-        return a+b
-
-maths=mathmetical_options("Shah Zaib")
-sum_num=maths.sum(11,19)
-concatenate_name=maths.sum("Shah ", "Zaib")
-print(f"Sum of two numbers: {sum_num}")
-print(f"Concatenate wo strings: {concatenate_name}")
-
