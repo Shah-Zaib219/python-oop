0641dae1e226dbf3818ed740bd57ea35b4d24173
Shah-Zaib219 <shahzaib63219@gmail.com>
2026-02-23 15:28:01 +0500

    update

--- main.py
+++ main.py
@@ -433,10 +433,12 @@ class mathmetical_options:
     def __init__(self, name):
         self.name=name
         print(f'Welcome {name}')
-    def sum(a,b):
+    def sum(self,a,b):
         return a+b
 
 maths=mathmetical_options("Shah Zaib")
 sum_num=maths.sum(11,19)
 concatenate_name=maths.sum("Shah ", "Zaib")
+print(f"Sum of two numbers: {sum_num}")
+print(f"Concatenate wo strings: {concatenate_name}")
 
